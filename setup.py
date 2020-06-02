c7a4d31522322e7632a8c948f77d054f472a39fc
Aykut Yilmaz <aykuxt@gmail.com>
2020-06-02 20:21:29 +0200

    Add dependencies for documentation

--- setup.py
+++ setup.py
@@ -38,4 +38,10 @@ setup(
         'mutagen==1.*,>=1.44.0', 'pycryptodome==3.*,>=3.9.7',
         'requests==2.*,>=2.23.0'
     ],
+    extras_require={
+        "docs": [
+            "mkdocs==1.*,>=1.1.2", "mkdocs-material==5.*,>=5.2.2",
+            "mkdocstrings==0.*,>=0.11.2"
+        ]
+    },
 )
