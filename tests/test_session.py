c4e0fd1ad62304e5a757b980e2b0c0517ae2a615
Aykut Yilmaz <aykuxt@gmail.com>
2020-06-02 17:10:35 +0200

    Add docstrings for tests

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,3 +1,4 @@
+"""This module contains tests for the [session][deethon.session] module."""
 from pathlib import Path
 
 import pytest
@@ -6,11 +7,17 @@ import deethon
 
 
 def test_session():
+    """
+    Test if the [Session][deethon.session.Session] class raises a
+    [DeezerLoginError][deethon.types.DeezerLoginError] when an invalid
+    arl token is passed.
+    """
     with pytest.raises(deethon.errors.DeezerLoginError):
         deethon.Session('wrongarltoken')
 
 
 def test_download():
+    """Test if the download method returns a Path object."""
     deezer = deethon.Session(
         '25e67e8e8100407765315a67e0d132fb88a436ffe244813b62ce7086c2efe01b3152041e4308dae45b14f22161adf525ede64cfa18eb9db01b02ec4ccac8869a7e726f372db54c313d4f5705637989ea1e20953a3ea60b5c0179ca95ddfe0516')
     assert isinstance(deezer.download('https://www.deezer.com/track/2104162', 'MP3_320'), Path)
--- tests/test_types.py
+++ tests/test_types.py
@@ -1,15 +1,26 @@
+"""This module contains tests for the types module."""
 import pytest
 
 import deethon
 
 
 def test_track():
+    """
+    Test if the [Track][deethon.types.Track] class raises a
+    [DeezerApiError][deethon.errors.DeezerApiError] when an invalid
+    track ID is passed.
+    """
     assert deethon.Track(95813354).id == 95813354
     with pytest.raises(deethon.errors.DeezerApiError):
         deethon.Track(1234567890)
 
 
 def test_album():
+    """
+    Test if the [Album][deethon.types.Album] class raises a
+    [DeezerApiError][deethon.errors.DeezerApiError] when an invalid
+    album ID is passed.
+    """
     assert deethon.Album(103248).id == 103248
     with pytest.raises(deethon.errors.DeezerApiError):
         deethon.Album(1234567890)
