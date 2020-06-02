094d15a8c7000f94b96accbc583785a16f9d8229
Aykut Yilmaz <aykuxt@gmail.com>
2020-06-02 15:34:43 +0200

    Fix __init__ method from base class 'Exception' is not called

--- deethon/errors.py
+++ deethon/errors.py
@@ -1,42 +1,34 @@
-"""
-The errors module contains all custom error classes
-"""
+"""The errors module contains all custom error classes"""
 
 
 class DeezerLoginError(Exception):
-    """Raised when login to Deezer fails."""
+    """Occurs when login to Deezer fails."""
 
-    def __str__(self) -> str:
-        return "Failed to login to Deezer. Please check your arl token."
+    def __init__(self):
+        super().__init__("Failed to login to Deezer. Please check your arltoken.")
 
 
 class DeezerApiError(Exception):
-    """Raised when a Deezer api request returns an error"""
+    """Occurs when a Deezer API request replies with an error"""
 
     def __init__(self, error: str, message: str, code: int):
         self.error: str = error
-        self.message: str = message
         self.code: int = code
-
-    def __str__(self) -> str:
-        return f"Error {self.code} - {self.message}"
+        self.message: str = message
+        super().__init__(f"Error {code}: {error} - {message}")
 
 
 class InvalidUrlError(Exception):
-    """Raised when an invalid url is passed"""
+    """Occurs when an invalid URL is passed"""
 
     def __init__(self, url: str):
         self.url: str = url
-
-    def __str__(self) -> str:
-        return f"{self.url} is not a valid Deezer url"
+        super().__init__(f'"{url}" is not a valid Deezer url.')
 
 
 class ActionNotSupported(Exception):
-    """Raised when an invalid action is called"""
+    """Occurs when an invalid action is called"""
 
     def __init__(self, mode: str):
         self.mode: str = mode
-
-    def __str__(self) -> str:
-        return f"Action is not (yet) supported: {self.mode}"
+        super().__init__(f"Action is not (yet) supported: {self.mode}")
