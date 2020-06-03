f0adf42e9a2cd58d5c31552c7c02ffa550131197
Aykut Yilmaz <aykuxt@gmail.com>
2020-06-03 20:57:12 +0200

    Move mkdocs to dev-dependencies

--- setup.py
+++ setup.py
@@ -42,7 +42,4 @@ setup(
         'mutagen==1.*,>=1.44.0', 'pycryptodome==3.*,>=3.9.7',
         'requests==2.*,>=2.23.0'
     ],
-    extras_require={
-        "docs": ["mkdocs-material==5.*,>=5.2.2", "mkdocstrings==0.*,>=0.11.2"]
-    },
 )
