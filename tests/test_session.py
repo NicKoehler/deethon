55cc9fc073f82f1dbe826f4bf9867ec8dac81985
Aykut Yilmaz <aykuxt@gmail.com>
2020-06-12 21:05:57 +0200

    Improve test coverage

--- tests/test_session.py
+++ tests/test_session.py
@@ -20,5 +20,16 @@ def test_download():
     """Test if the download method returns a Path object."""
     deezer = deethon.Session(
         '25e67e8e8100407765315a67e0d132fb88a436ffe244813b62ce7086c2efe01b3152041e4308dae45b14f22161adf525ede64cfa18eb9db01b02ec4ccac8869a7e726f372db54c313d4f5705637989ea1e20953a3ea60b5c0179ca95ddfe0516')
-    assert isinstance(deezer.download('https://www.deezer.com/track/2104162', 'MP3_320'), Path)
-    assert isinstance(deezer.download('https://www.deezer.com/track/2104162', 'FLAC'), Path)
+    assert isinstance(
+        deezer.download('https://www.deezer.com/track/2104162', 'MP3_320'), Path)
+    assert isinstance(
+        deezer.download('https://www.deezer.com/track/2104162'), Path)
+
+    with pytest.raises(deethon.errors.InvalidUrlError):
+        deezer.download('https://www.google.com')
+
+    with pytest.raises(deethon.errors.ActionNotSupported):
+        deezer.download('https://www.deezer.com/unsuppoted/123456')
+
+    with pytest.raises(deethon.errors.DownloadError):
+        deezer.download('https://www.deezer.com/track/101399924')
--- tests/test_types.py
+++ tests/test_types.py
@@ -21,6 +21,18 @@ def test_album():
     [DeezerApiError][deethon.errors.DeezerApiError] when an invalid
     album ID is passed.
     """
-    assert deethon.Album(103248).id == 103248
+    album = deethon.Album(103248)
+    assert album.id == 103248
+
+    # Test album covers
+    assert isinstance(album.cover_small, bytes)
+    assert isinstance(album.cover_medium, bytes)
+    assert isinstance(album.cover_big, bytes)
+    assert isinstance(album.cover_xl, bytes)
+
+    # Test caching
+    assert album is deethon.Album(103248)
+
+    # Test errors
     with pytest.raises(deethon.errors.DeezerApiError):
         deethon.Album(1234567890)
